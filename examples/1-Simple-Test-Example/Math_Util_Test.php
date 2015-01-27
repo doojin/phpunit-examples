3027dcc76e3933edfdf8f0615656fd26468400ae
Dmitry Papka <dmitry.papka@gmail.com>
2015-01-27 16:02:18 +0000

    Methods for Simple test example renamed

--- examples/1-Simple-Test-Example/Math_Util_Test.php
+++ examples/1-Simple-Test-Example/Math_Util_Test.php
@@ -4,7 +4,7 @@ require_once 'Math_Util.php';
 
 class Math_Util_Test extends PHPUnit_Framework_TestCase
 {
-    public function test_add_shouldReturnSumOfTwoPositiveNumbers()
+    public function test__add__should_return_sum_of_two_positive_numbers()
     {
         $expected = 5.1;
         
@@ -13,7 +13,7 @@ class Math_Util_Test extends PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $result);
     }
     
-    public function test_add_shouldReturnSumOfNegativeNumbers()
+    public function test__add__return_sum_of_two_negative_numbers()
     {
         $expected = -5.1;
         
